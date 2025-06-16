7416a61a47303ec882bfb71c690e69b1325603a9
Pranjali <pranjalisr25@gmail.com>
2025-06-16 23:08:37 +0530

    init

--- June/potd116.java
+++ June/potd116.java
@@ -2,7 +2,8 @@
 class Solution {
     public int maximumDifference(int[] nums) {
         int xMin = nums[0];
-        int maxD = -1;
+        int maxD = -1;    // This will store the largest difference between a later number and a smaller earlier number. If no such pair is found (i.e., the array is non-increasing), the return value remains -1.
+        
 
         for (int i = 1; i < nums.length; i++) {
             int x = nums[i];
